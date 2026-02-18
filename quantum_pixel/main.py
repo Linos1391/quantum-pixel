0ecee35619065b70aa8a4318b697d55b593efa56
Linos1391 <linos.coding@gmail.com>
2026-02-18 15:45:40 -0500

    degrade python to 3.11

--- quantum_pixel/main.py
+++
@@ -1,9 +0,0 @@
-"""Run website as CLI."""
-
-import os
-
-import uvicorn
-from . import FastAPIApp
-
-if __name__ == "__main__":
-    uvicorn.run(FastAPIApp, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
