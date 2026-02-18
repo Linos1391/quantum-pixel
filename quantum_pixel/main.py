88e9d0beff0fac76e0a3bfb5f1425db0157fe9db
Linos1391 <linos.coding@gmail.com>
2026-02-18 14:55:44 -0500

    whatever, undone changes

--- quantum_pixel/main.py
+++ quantum_pixel/main.py
@@ -1,27 +1,9 @@
 """Run website as CLI."""
 
 import os
-import sys
-import asyncio
 
 import uvicorn
 from . import FastAPIApp
 
-async def test():
-    """Open website and shutdown after 10s."""
-    config = uvicorn.Config(FastAPIApp, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
-    server = uvicorn.Server(config)
-
-    # Run the server in a background task
-    task = asyncio.create_task(server.serve())
-
-    await asyncio.sleep(10)
-    # Gracefully shut down the server
-    await server.shutdown()
-    await task
-
 if __name__ == "__main__":
-    if sys.argv[1] == "test":
-        asyncio.run(test())
-    else:
-        uvicorn.run(FastAPIApp, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
+    uvicorn.run(FastAPIApp, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
